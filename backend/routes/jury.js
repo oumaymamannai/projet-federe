1910d99bf4d0fd0d1b46c592c31965864636ce39
oumaymamannai <oumaymamannai@etudiant-fst.utm.tn>
2026-03-31 18:09:59 +0200

    jury.js

--- backend/routes/jury.js
+++ backend/routes/jury.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const { verifyToken, requireRole } = require("../middleware/auth");
 const ctrl = require("../controllers/adminController"); // Utilise le même contrôleur
 
+const juryController = require("../controllers/juryController");
 // Appliquer les middlewares (admin uniquement pour ces routes)
 router.use(verifyToken, requireRole("admin"));
-
+router.use(verifyToken, requireRole("jury"));
 // Routes pour la gestion des membres du jury
 router.get("/members", ctrl.getAllJuryMembers);           // GET /api/jury/members
 router.post("/contact", ctrl.sendEmailToMember);          // POST /api/jury/contact
@@ -16,5 +17,10 @@ router.post("/:soutenance_id/affecter", ctrl.affecterJury);  // POST /api/jury/:
 
 // Route pour récupérer les membres du jury (simple liste)
 router.get("/list", ctrl.getJuryMembers);                 // GET /api/jury/list
+
+
+router.get("/dashboard", juryController.getDashboard);           // GET /api/jury/members
+router.get("/soutenances", juryController.getMesSoutenances);          // POST /api/jury/contact
+router.post("/evaluer/:soutenance_id", juryController.evaluer); 
 
 module.exports = router;
